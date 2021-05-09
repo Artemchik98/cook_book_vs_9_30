bb03b82399c07f5c90279c3f90425fca5aa1ca46
Artem_Svistelnik <artem.svistelnik@gmail.com>
2021-05-09 09:16:27 +0300

    before lesson 9.05.2021 commit

--- blog/models.py
+++ blog/models.py
@@ -16,4 +16,36 @@ class Post(models.Model):
                          related_name='blog_posts',verbose_name='Автор')
     short_description=models.CharField(max_length=400,verbose_name='Короткое описание')
     publish=models.DateTimeField(default=timezone.now,verbose_name='Дата публикации')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft', verbose_name='Статус публикации')
+    image = models.ImageField(upload_to='product_images/', blank=False, verbose_name='Изображение')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                             self.publish.month,
+                                             self.publish.day,
+                                             self.slug])
+
+
+
+
+def save_images(instance, filename):
+    post_id = instance.post.id
+    return 'gallery_images/{}/{}'.format(post_id, filename)
+
+
+class PostPoint(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
+    post_point_text = models.TextField(verbose_name='Пункт поста')
+    post_images = models.ImageField(upload_to=save_images, blank=True, verbose_name='Изображение пункта',)
+
+    def __str__(self):
+        return 'Пункт поста {}'.format(self.post.title)
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Post,PostPoint
+from django.core.paginator import  Paginator,EmptyPage,PageNotAnInteger
+
+
+def post_list(request):
+    object_list=Post.objects.all()
+    paginator=Paginator(object_list,1) #10 постов = 10 страниц
+    page=request.GET.get('page')
+    try:
+        posts=paginator.page(page)# пост №5
+    except PageNotAnInteger: # Если постов на страницу по 10 а всего их 5
+        posts=paginator.page(1)
+    except EmptyPage:#если номер страницы которой нет
+        posts=paginator.page(paginator.num_pages)
+
+    return render(request,'blog/post/list.html',{'page':page,
+                                                'posts':posts})
+
+
+
+
+def post_detail(request,year,month,day,slug):
+    post_object=get_object_or_404(Post, status='published',
+                                        slug=slug,
+                                        publish__year=year,
+                                        publish__month=month,
+                                        publish__day=day)
+
+    post_points=PostPoint.objects.filter(post=post_object)
+    return render(request,'blog/post/detail.html',{'post':post_object,
+                                                   'post_points':post_points})
